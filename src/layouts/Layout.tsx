2ac7802fb6f1c1e454c6b4642942e6b5e3d8709b
YahyaNaq <yahya.naqvi123@gmail.com>
2024-11-21 23:10:36 +0500

    removed comment

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -39,44 +39,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      {/* <Sheet>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Products</SheetTitle>
-            <SheetDescription>
-              Make changes to your profile here. Click save when you're done.
-            </SheetDescription>
-            <ul>
-
-            </ul>
-            {items.map((item) => (
-                <li key={item.title}>
-                  <a href={item.url}>
-                    <item.icon  className="text-slate-600 font-semibold"/>
-                    <span className="text-slate-600 font-semibold">{item.title}</span>
-                  </a>
-                </li>
-              ))}
-          </SheetHeader>
-          <div className="grid gap-4 py-4">
-
-          </div>
-        </SheetContent>
-        <Header sidebarTrigger={
-          <SheetTrigger asChild>
-            <Button variant="outline">
-              <Menu />
-              <span className="hidden lg:block">All Products</span>
-              <span className="hidden md:block lg:hidden">All</span>
-            </Button>
-          </SheetTrigger>
-        }
-        />
-      </Sheet>
-      <main className="max-w-screen mt-20 md:mx-8 mx-4">
-        {children}
-      </main>
-      <Footer /> */}
       <SidebarProvider>
         <Header sidebarTrigger = {
           <SidebarTrigger className="m-2 z-20 shadow-none" variant="secondary" title="All Products" size="sm" />
