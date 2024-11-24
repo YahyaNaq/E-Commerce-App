401b972d3039901b8624cfb99fd015ca6ffe1f1d
YahyaNaq <yahya.naqvi123@gmail.com>
2024-11-24 18:57:07 +0500

    resolved ts errors

--- src/layouts/BreadCrumb.tsx
+++ src/layouts/BreadCrumb.tsx
@@ -3,7 +3,7 @@ import {
     BreadcrumbItem,
     BreadcrumbLink,
     BreadcrumbList,
-    BreadcrumbPage,
+    // BreadcrumbPage,
     BreadcrumbSeparator,
   } from "@/components/ui/breadcrumb"
 import React from "react";
